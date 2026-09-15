21d88c6b2a4c8185388931fb875f741062564c52
LeeChaeBin002 <dlcoqls002@gmail.com>
2026-09-15 14:33:49 +0900

    [Passed] 음수 구별하기 (68ms, 5MB)

---
+++ trail1/음수 구별하기/separate-negative-number.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int N =int.Parse(Console.ReadLine());
+        Console.WriteLine(N);   
+
+        if(N<0)
+        {
+            Console.WriteLine("minus");
+        }
+
+        // Please write your code here.
+    }
+}
