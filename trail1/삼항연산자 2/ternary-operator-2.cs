3969bdfb410f3bb38414578d5bafba8f676662b4
LeeChaeBin002 <dlcoqls002@gmail.com>
2026-09-15 15:50:47 +0900

    [Passed] 삼항연산자 2 (64ms, 5MB)

---
+++ trail1/삼항연산자 2/ternary-operator-2.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int a =int.Parse(Console.ReadLine());
+
+        char result;
+        result= a ==1 ? 't' : 'f';
+        Console.WriteLine(result);
+        // Please write your code here.
+    }
+}
