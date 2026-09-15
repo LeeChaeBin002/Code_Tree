24244eb7fc793e84ad3dcc2fb0103f715813c569
LeeChaeBin002 <dlcoqls002@gmail.com>
2026-09-15 15:18:50 +0900

    [Passed] 정사각형의 넓이 (89ms, 5MB)

---
+++ trail1/정사각형의 넓이/area-of-a-rectangle.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        float N = float.Parse(Console.ReadLine());
+        Console.WriteLine(N*N);
+        if(N<5)
+        {
+            Console.WriteLine("tiny");
+        } 
+
+        // Please write your code here.
+    }
+}
