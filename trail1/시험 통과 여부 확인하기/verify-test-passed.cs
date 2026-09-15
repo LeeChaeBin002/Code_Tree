4a9dd12f3050d71a90a40bb96bc945753d22d683
LeeChaeBin002 <dlcoqls002@gmail.com>
2026-09-15 15:25:26 +0900

    [Passed] 시험 통과 여부 확인하기 (68ms, 5MB)

---
+++ trail1/시험 통과 여부 확인하기/verify-test-passed.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int N =int.Parse(Console.ReadLine());
+
+        if(N>=80)
+        {
+            Console.WriteLine("pass");
+        }
+        else
+        {
+            Console.WriteLine($"{80-N} more score");
+        }
+        // Please write your code here.
+    }
+}
