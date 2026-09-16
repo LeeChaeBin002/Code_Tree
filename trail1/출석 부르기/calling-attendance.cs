37018af394b202d4092b9a14535faf2e9de0cab8
LeeChaeBin002 <dlcoqls002@gmail.com>
2026-09-16 17:15:11 +0900

    [Passed] 출석 부르기 (67ms, 6MB)

---
+++ trail1/출석 부르기/calling-attendance.cs
@@ -0,0 +1,27 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int s =int.Parse(Console.ReadLine());
+
+        if(s ==1)
+        {
+            Console.WriteLine("John");
+        }
+        else if(s==2)
+        {
+            Console.WriteLine("Tom");
+        }
+         else if(s==3)
+        {
+            Console.WriteLine("Paul");
+        }
+        else
+        {
+            Console.WriteLine("Vacancy");
+        }
+        // Please write your code here.
+    }
+}
