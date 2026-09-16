3ca88d699c98c95546c9a18ba2bdb3e4b5f0388d
LeeChaeBin002 <dlcoqls002@gmail.com>
2026-09-16 18:08:54 +0900

    [Passed] 최대 2번의 연산 (63ms, 5MB)

---
+++ trail1/최대 2번의 연산/up-to-2-calculations.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+
+        if(a % 2 == 0)
+        {
+            a= a/2;
+        }
+        if(a % 2 == 1)
+        {
+            a= (a+1)/2;
+        }   
+
+        Console.WriteLine(a);
+        // Please write your code here.
+    }
+}
