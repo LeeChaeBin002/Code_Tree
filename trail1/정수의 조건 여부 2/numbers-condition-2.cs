852bbc6b3bbe5f617b997ec386ee1c6f9f3a87af
LeeChaeBin002 <dlcoqls002@gmail.com>
2026-09-16 18:13:15 +0900

    [Passed] 정수의 조건 여부 2 (66ms, 5MB)

---
+++ trail1/정수의 조건 여부 2/numbers-condition-2.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+
+        if(a==5)
+        {
+            Console.WriteLine('A');
+        }
+        else if(a % 2== 0)
+        {
+            Console.WriteLine('B');
+        }
+        else
+        {
+            Console.WriteLine();
+        }
+        // Please write your code here.
+    }
+}
