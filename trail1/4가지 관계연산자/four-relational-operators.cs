967622a0ef101436ed5e4e36cf2cdee6a4e65744
LeeChaeBin002 <dlcoqls002@gmail.com>
2026-09-17 20:20:57 +0900

    [Passed] 4가지 관계연산자 (69ms, 7MB)

---
+++ trail1/4가지 관계연산자/four-relational-operators.cs
@@ -0,0 +1,48 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        string s = Console.ReadLine();
+        string[] parts = s.Split();
+
+        int A = int.Parse(parts[0]);
+        int B = int.Parse(parts[1]);
+
+        if(A >= B)
+        {
+            Console.WriteLine('1');
+        }
+        else
+        {
+            Console.WriteLine('0');
+        }
+        if(A > B)
+        {
+            Console.WriteLine('1');
+        }
+        else
+        {
+            Console.WriteLine('0');
+        }    
+        if(A <= B)
+        {
+            Console.WriteLine('1');
+        }
+        else
+        {
+            Console.WriteLine('0');
+        }            
+        if(A < B)
+        {
+            Console.WriteLine('1');
+        }
+        else
+        {
+            Console.WriteLine('0');
+        }    
+
+
+        }
+}
