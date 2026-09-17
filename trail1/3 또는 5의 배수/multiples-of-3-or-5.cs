f6d5f3c03c7e961a6808e418fea68f3f93e51b23
LeeChaeBin002 <dlcoqls002@gmail.com>
2026-09-17 19:22:07 +0900

    [Passed] 3 또는 5의 배수 (63ms, 5MB)

---
+++ trail1/3 또는 5의 배수/multiples-of-3-or-5.cs
@@ -0,0 +1,27 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int A = int.Parse(Console.ReadLine());
+
+        if(A % 3 == 0 )
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }    
+        if(A % 5 == 0)
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
+        // Please write your code here.
+    }
+}
