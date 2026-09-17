0cf0a69c33d37824d1f489b0785f84eca6502099
LeeChaeBin002 <dlcoqls002@gmail.com>
2026-09-17 21:35:49 +0900

    [Passed] 범위 밖의 수 (63ms, 5MB)

---
+++ trail1/범위 밖의 수/number-out-of-range.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+
+        if(a<10 || a>20)
+        {
+            Console.WriteLine("yes");
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+            
+        // Please write your code here.
+    }
+}
