5236b488f0217fe04dae1fcaa00c8fe41fe0aa46
LeeChaeBin002 <dlcoqls002@gmail.com>
2026-09-19 00:29:57 +0900

    [Passed] 입력받는 수 부터 100까지 출력 (65ms, 5MB)

---
+++ trail1/입력받는 수 부터 100까지 출력/print-number-from-given-num-to-100.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int n =  int.Parse(Console.ReadLine());
+
+        for(int i = n ; i <= 100 ; i++)
+        {
+            Console.Write($"{i} ");
+        }
+        
+        // Please write your code here.
+    }
+}
