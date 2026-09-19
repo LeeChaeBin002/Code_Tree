4dcd02d42f484d8b86d64e246eeb726989fa143f
LeeChaeBin002 <dlcoqls002@gmail.com>
2026-09-19 16:11:12 +0900

    [Passed] 1부터 n까지 출력 (69ms, 5MB)

---
+++ trail1/1부터 n까지 출력/print-from-1-to-n.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+
+        int N =int.Parse(Console.ReadLine());
+
+        for (int i = 1 ; i <= N; i++)
+        {
+            Console.Write(i);
+            Console.Write(" ");
+        }
+        // Please write your code here.
+    }
+}
