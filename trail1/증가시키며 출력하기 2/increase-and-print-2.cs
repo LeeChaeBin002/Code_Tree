89ae675c4ec83d32a1db2e0e052498a30d4a09f5
LeeChaeBin002 <dlcoqls002@gmail.com>
2026-09-20 21:40:58 +0900

    [Passed] 증가시키며 출력하기 2 (57ms, 4MB)

---
+++ trail1/증가시키며 출력하기 2/increase-and-print-2.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int i = 10;
+        while(i<=26)
+        {
+            Console.Write($"{i++} ");
+        }
+        // Please write your code here.
+    }
+}
