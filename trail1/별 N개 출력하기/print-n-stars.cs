64f889318a60745bd507cb0136353ee3f2512076
LeeChaeBin002 <dlcoqls002@gmail.com>
2026-09-20 22:38:50 +0900

    [Passed] 별 N개 출력하기 (65ms, 5MB)

---
+++ trail1/별 N개 출력하기/print-n-stars.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int i = 0;
+        while(i < n)
+        {
+            i++;
+            Console.WriteLine("*");
+        }
+        // Please write your code here.
+    }
+}
