55ad50527b27bd473f1912b35c4a5b02ea1e27f3
LeeChaeBin002 <dlcoqls002@gmail.com>
2026-09-20 20:26:21 +0900

    [Passed] 특정 숫자 도달하기 2 (71ms, 11MB)

---
+++ trail1/특정 숫자 도달하기 2/reaching-specific-number-2.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+
+        for(int i = N; 1 <= i; i--)
+        {   
+            Console.Write($"{i} ");
+        }
+        // Please write your code here.
+    }
+}
