48d6068d5b2a819c748be4edd81c16e0f0ed3eba
LeeChaeBin002 <dlcoqls002@gmail.com>
2026-09-21 01:07:09 +0900

    [Passed] 감소시키며 출력하기 2 (58ms, 5MB)

---
+++ trail1/감소시키며 출력하기 2/decrease-and-print-2.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int i = 26;
+        while(i >=10)
+        {
+            Console.Write($"{i} ");
+            i--;
+        }
+        // Please write your code here.
+    }
+}
