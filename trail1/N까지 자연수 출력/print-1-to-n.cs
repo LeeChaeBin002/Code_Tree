c9e924381817dfe0c3528b2f78a06788a3f614bf
LeeChaeBin002 <dlcoqls002@gmail.com>
2026-09-20 22:11:05 +0900

    [Passed] N까지 자연수 출력 (74ms, 6MB)

---
+++ trail1/N까지 자연수 출력/print-1-to-n.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+
+        int i = 1;
+
+        while(N >= i)
+        {
+           
+            Console.Write($"{i++} ");
+        }
+        // Please write your code here.
+    }
+}
