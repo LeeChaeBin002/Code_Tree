92077bf2a45278de85abbb29ca180092ae3743e3
LeeChaeBin002 <dlcoqls002@gmail.com>
2026-09-21 01:33:44 +0900

    [Passed] n부터 1까지 정수 출력 (68ms, 5MB)

---
+++ trail1/n부터 1까지 정수 출력/print-n-to-1.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+    
+        while (1 <= n)
+        {
+            Console.Write($"{n} ");
+            n--;
+        }
+        // Please write your code here.
+    }
+}
