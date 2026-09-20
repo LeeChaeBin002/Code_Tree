3a31962a3b7b1e84f72278f9e479bbcb5de4f678
LeeChaeBin002 <dlcoqls002@gmail.com>
2026-09-21 03:40:24 +0900

    [Passed] 문자 출력하기 (55ms, 5MB)

---
+++ trail1/문자 출력하기/print-text.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        string s = Console.ReadLine();
+
+        for(int i =1; i <= 8; i++)
+        {
+            Console.Write(s);
+        }
+                
+        // Please write your code here.
+    }
+}
